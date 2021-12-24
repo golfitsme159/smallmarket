f905e1382944022776560404af1cc1ae72937af9
Golfitsme <golfitsme@gmail.com>
2021-12-24 21:17:59 +0700

    UPdate

--- lib/states/login.dart
+++ lib/states/login.dart
@@ -11,15 +11,87 @@ class Login extends StatefulWidget {
 }
 
 class _LoginState extends State<Login> {
+  bool statusRedEye = true;
+
   @override
   Widget build(BuildContext context) {
     double size = MediaQuery.of(context).size.width;
     return Scaffold(
       body: SafeArea(
         child: ListView(
-          children: [buildImage(size), buildAppName()],
+          children: [
+            buildImage(size),
+            buildAppName(),
+            buildUser(size),
+            buildPassword(size),
+          ],
         ),
       ),
+    );
+  }
+
+  Row buildUser(double size) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        Container(
+          margin: EdgeInsets.only(
+            top: 16,
+          ),
+          width: size * 0.6,
+          child: TextFormField(
+            decoration: InputDecoration(
+              labelStyle: MyConstant().h3Stlye(),
+              labelText: 'Email',
+              prefixIcon: Icon(
+                Icons.account_circle_outlined,
+                color: MyConstant.dart,
+              ),
+              enabledBorder: OutlineInputBorder(
+                borderSide: BorderSide(color: MyConstant.dart),
+                borderRadius: BorderRadius.circular(30),
+              ),
+              focusedBorder: OutlineInputBorder(
+                borderSide: BorderSide(color: MyConstant.light),
+                borderRadius: BorderRadius.circular(30),
+              ),
+            ),
+          ),
+        ),
+      ],
+    );
+  }
+
+  Row buildPassword(double size) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        Container(
+          margin: EdgeInsets.only(
+            top: 16,
+          ),
+          width: size * 0.6,
+          child: TextFormField(
+            obscureText: statusRedEye,
+            decoration: InputDecoration(
+              labelStyle: MyConstant().h3Stlye(),
+              labelText: 'Password',
+              prefixIcon: Icon(
+                Icons.password_outlined,
+                color: MyConstant.dart,
+              ),
+              enabledBorder: OutlineInputBorder(
+                borderSide: BorderSide(color: MyConstant.dart),
+                borderRadius: BorderRadius.circular(30),
+              ),
+              focusedBorder: OutlineInputBorder(
+                borderSide: BorderSide(color: MyConstant.light),
+                borderRadius: BorderRadius.circular(30),
+              ),
+            ),
+          ),
+        ),
+      ],
     );
   }
 
